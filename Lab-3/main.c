edfa86794e0d6e5510fa47b560dbeca9da21e5a3
TheSeaUrchin <71513698+TheSeaUrchin@users.noreply.github.com>
2024-04-23 19:16:41 -0400

    Update main.c

--- Lab-3/main.c
+++ Lab-3/main.c
@@ -43,13 +43,14 @@ void stoptimerA2(void);
 //Global vars
 
 int currentState = RUNMODE;
-int delay = 0;
-int timeoutReached = 0;
+
 unsigned int in_value = 0;
 unsigned int prev_in_value = 0;
-int startMonth = 4;
+int startMonth = 3;
 int startDay = 17;
 long unsigned int timer = 0;
+int currentMonth = 3;
+int monthOffset[12] = {1,-1,1,0,1,0,1,1,0,1,0,1};
 int tempStore[30];
 
 int main(void) {
@@ -60,12 +61,30 @@ int main(void) {
 
 
     //timer Stuff
-    timer = startMonth * 2628288 + startDay * 86400;
+    timer = startMonth * 2592000 + startDay * 86400;
     _enable_interrupt();
     _BIS_SR(GIE);
     initTimer_A();
     wheelSetup();
+    int buttonReleased = 0;
 
+    //Define underline location for when editing time
+    //{x1,x2}
+    int monthCords[2] = {3,18};
+    int dayCords[2] = {26,38};
+    int hourCords[2] = {46,58};
+    int minCords[2] = {62,74};
+    int secCords[2] = {80,92};
+    int (*coords[5])[2] = {monthCords,dayCords,hourCords,minCords,secCords};
+
+    //Define time increase values
+    unsigned long int times[5] = {2592000,86400,3600,60,1};
+    //unsigned long int times[5] = {2505600,86400,3600,60,1};
+
+
+
+    int clicks=0;
+    long unsigned int timeAdd = 0;
     while(1){
 
         //Regular clock runtime
@@ -73,33 +92,75 @@ int main(void) {
             if(getLeftButton() == 1){
                 currentState = EDITMODE;
                 stoptimerA2();
+                clicks = 0;
+                timeAdd=times[0];
+                Graphics_drawLineH(&g_sContext, (*coords[clicks])[0],(*coords[clicks])[1], 20);
+                Graphics_flushBuffer(&g_sContext);
+                pollWheel(); //get an initial previous value
+                prev_in_value = in_value;
             }
         }
 
+
+
         //Go into edit mode with scroll wheel
         else {
+            if(getLeftButton() == 1 && buttonReleased == 1){
+                buttonReleased = 0;
+                clicks++;
+                if(clicks>4){
+                    clicks = 0;
+                }
+                timeAdd = times[clicks];
+
+                //clear line and draw new line
+                Graphics_setForegroundColor(&g_sContext,GRAPHICS_COLOR_WHITE);
+                Graphics_drawLineH(&g_sContext, (*coords[0])[0],(*coords[4])[1], 20);
+                Graphics_setForegroundColor(&g_sContext,GRAPHICS_COLOR_BLACK);
+                Graphics_drawLineH(&g_sContext, (*coords[clicks])[0],(*coords[clicks])[1], 20);
+                Graphics_flushBuffer(&g_sContext);
+            }
+            else if(getLeftButton() == 0){
+                buttonReleased = 1;
+            }
             //Use to detect differences in scroll value
-            prev_in_value = in_value;
+
 
             pollWheel(); //blocking function
 
-            displayWheel(in_value); //Used for debug purposes
+
 
             //Modify day, will later implement for other time aspects
 
-            if(in_value> 4082 || prev_in_value+50<in_value){
-                timer += 86400;
+            if(in_value> 4082 || prev_in_value+80<in_value){
+                if(clicks == 0){
+                    timeAdd= times[0] + monthOffset[currentMonth]*times[1];
+                }
+                timer += timeAdd;
                 displayTime(timer);
+                prev_in_value = in_value;
             }
-            else if(prev_in_value-50>in_value){
-                timer -= 86400;
+            else if(prev_in_value-80>in_value){
+                if(clicks == 0){
+                    timeAdd= times[0] + monthOffset[currentMonth-1]*times[1];
+                }
+                timer -= timeAdd;
                 displayTime(timer);
+                prev_in_value = in_value;
             }
+
 
             //Exit edit mode
             if(getRightButton() == 1){
                 currentState = RUNMODE;
                 initTimer_A();
+
+                //clear underline
+                Graphics_setForegroundColor(&g_sContext,GRAPHICS_COLOR_WHITE);
+                Graphics_drawLineH(&g_sContext, (*coords[0])[0],(*coords[4])[1], 20);
+                Graphics_setForegroundColor(&g_sContext,GRAPHICS_COLOR_BLACK);
+
+                displayTime(timer);
             }
 
         }
@@ -146,7 +207,7 @@ void buttonConfig(){
     P2REN |= (BIT1);
     P2OUT |= (BIT1);
 
-    P1SEL = P3SEL & ~BIT1;
+    P1SEL = P1SEL & ~BIT1;
     P1DIR |= BIT1;
     P1REN |= BIT1;
     P1OUT |= BIT1;
@@ -165,22 +226,34 @@ int getRightButton(){
 void displayTime(long unsigned int inTime){
 //    int startMonth = 3;
 //    int startDay = 17;
-    int monthOffset[12] = {1,-1,1,0,1,0,1,1,0,1,0,1};
+
+    if(inTime>500*86400){
+        inTime = 365*86400 - (0xffffffff - inTime) -1;
+        timer = inTime;
+    }
+
     char months[12][3] = {"JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"};
     long unsigned int sec = inTime%60;
     long unsigned int min = inTime/60;
     long unsigned int hr = min/60;
     long unsigned int day = hr/24;
+    hr = hr%24;
+    min = min%60;
+
     int month = 0;
     while(day>30+monthOffset[month]){
         day-=(30+monthOffset[month]);
         month++;
-
-
+        if(month>11){
+            month = 0;
+            timer-= 366 * 86400;
+        }
     }
+    currentMonth = month;
 
-    hr = hr%24;
-    min = min%60;
+
+
+
 
 
 
@@ -203,9 +276,9 @@ void displayTime(long unsigned int inTime){
 
 
     char output[16] = "000 00 00:00:00";
-    output[0]=months[month][0];
-    output[1]=months[month][1];
-    output[2]=months[month][2];
+    output[0]=months[currentMonth][0];
+    output[1]=months[currentMonth][1];
+    output[2]=months[currentMonth][2];
     output[4]=dayStr[0];
     output[5]=dayStr[1];
     output[7]=hrStr[0];
@@ -254,10 +327,11 @@ void displayWheel(int wheelIn){
     wheelStr[2] = (wheelIn % 1000)/100 + 0x30;
     wheelStr[3] = (wheelIn % 100)/10 + 0x30;
     wheelStr[4] = (wheelIn % 10) + 0x30;
-    Graphics_drawStringCentered(&g_sContext,wheelStr, 5, 48, 45, OPAQUE_TEXT);
-    Graphics_flushBuffer(&g_sContext);
+//    Graphics_drawStringCentered(&g_sContext,wheelStr, 5, 48, 45, OPAQUE_TEXT);
+//    Graphics_flushBuffer(&g_sContext);
 
 }
+
 
 void wheelSetup(void){
     // Configure P8.0 as digital IO output and set it to 1
@@ -289,8 +363,10 @@ void pollWheel(void){
 
 
     // Poll busy bit waiting for conversion to complete
-    while (ADC12CTL1 & ADC12BUSY)
+    while (ADC12CTL1 & ADC12BUSY){
         __no_operation();
+    }
+
     in_value = ADC12MEM0;               // Read results if conversion done
 }
 
